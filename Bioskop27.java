2446de4ab82d4116ec21710a7cec687cdd5f8290
sandyy221 <kurniawansandy808.com>
2024-11-07 08:48:01 +0700

    percobaan1

--- Bioskop27.java
+++ Bioskop27.java
@@ -23,8 +23,8 @@ public class Bioskop27 {
         }
 
         System.out.println("Penonton baris ke-3: ");
-        for (String i : penonton[2]){
-            System.out.println(i);
+        for (int i= 0; i<penonton.length; i++){
+            System.out.println("Penonton pada baris ke- "+(i+1) + ": " + String.join(",", penonton[i]));
         }
     }
     
